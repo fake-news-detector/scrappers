d55708f4929b8379915e4420fee78780aa5e8e6c
Rogerio Chaves <rogeriocfj@gmail.com>
2018-05-22 22:14:54 -0300

    Loop through all pages scrapping news

--- scrappers/buzzfeed_com.py
+++ scrappers/buzzfeed_com.py
@@ -6,12 +6,13 @@ import os
 
 def get_soup(url):
     page = requests.get(url)
+    page.raise_for_status()
     return BeautifulSoup(page.text, 'html.parser')
 
 
 def find_titles_from_category_page(category, page):
     soup = get_soup('https://www.buzzfeed.com/br/feedpage/feed/search_buzzes?page=' +
-                    str(page) + '&page_name=badge&tags.tag_name=lol')
+                    str(page) + '&page_name=badge&tags.tag_name=' + category)
     titles = [{'title': h2.text,
                'url': 'https://www.buzzfeed.com' + h2.parent['href']}
               for h2 in soup.select('h2')]
@@ -27,3 +28,25 @@ def append_to_csv(filename, items):
     with open(filename, "a") as myfile:
         lines = [",".join(item.values()) for item in items]
         myfile.write(head + "\n" + "\n".join(lines))
+
+
+def scrape_all_pages(category, filename):
+    initial_page = 1
+    final_page = 50
+
+    print('Scrapping category', category)
+    for page in range(initial_page, final_page):
+        print('Scrapping page', page)
+        try:
+            titles = find_titles_from_category_page(category, page)
+            append_to_csv(filename, titles)
+        except requests.exceptions.HTTPError:
+            print('Page %s not found' % page)
+            break
+
+
+if __name__ == "__main__":
+    for category in ['lol', 'wtf', 'omg', 'cute']:
+        scrape_all_pages(category, "clickbait_titles.csv")
+
+    scrape_all_pages("newsbr", "non_clickbait_titles.csv")
